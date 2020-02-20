26259bc40aab11319874756b1c81252a18ff9f3c
romiagungn <romian1394@gmail.com>
2020-02-20 22:57:59 +0700

    challenge18 page mahasiswa

--- challenge18/challenge18.js
+++ challenge18/challenge18.js
@@ -41,3 +41,57 @@ JL. Setiabudi No.255
             })
         })
     })
+
+function mainmenu() {
+    console.log(`
+=========================================================
+silahkan pilih opsi dibawah ini
+[1] Mahasiswa
+[2] Jurusan
+[3] Dosen
+[4] Mata Kuliah
+[5] Kontrak
+[6] Keluar
+=========================================================`);
+    rl.question(`masukkan salah satu no. dari opsi diatas : `, (number) => {
+        switch (number) {
+            case '1':
+                mahasiswa();
+                break;
+            case '2':
+                jurusan();
+                break;
+        }
+    })
+}
+
+function mahasiswa() {
+console.log(`
+=========================================================
+silahkan pilih opsi dibawah ini
+[1] Daftar Murid
+[2] Cari Murid
+[3] Tambah Murid
+[4] Hapus Murid
+[5] Keluar
+=========================================================`);
+    rl.question(`masukkan salah satu no. dari opsi diatas : `, (number) => {
+        console.log('=========================================================')
+        switch (number) {
+            case '1':
+                daftarmurid();
+                break;
+            case '2':
+                carimurid();
+                break;
+            case '3':
+                tambahmurid();
+                break;
+            case '4':
+                deletemurid();
+                break;
+            case '5':
+                mainmenu();
+        }
+    })
+};
